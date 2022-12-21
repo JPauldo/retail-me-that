43e98d103ab73ed1679316869c0f51b6e6dedef3
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2022-12-21 12:12:52 -0500

    Added sequilize to the server

--- models/index.js
+++ models/index.js
@@ -4,25 +4,25 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: 'category_id'
-})
-
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE'
 })
 
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
+
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: 'ProductTag'
+  through: ProductTag
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: 'ProductTag'
+  through: ProductTag
 })
 
 module.exports = {
